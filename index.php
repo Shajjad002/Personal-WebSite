c1e8c35b92025f5b1b3bca241e01b7a21a209020
Shariful Islam <90764070+Shajjad002@users.noreply.github.com>
2023-02-13 21:12:09 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,352 @@
+<?php
+	include('include/head.php');
+	include('include/header.php');
+?>
+	<!--Home section Start-->
+	<div class="main-content">
+		<section class="home section p-3  " id="home">
+			<div class="container">
+				<div class="row">
+					<div class="home-info col-8 col-sm-12">
+						<h3 class="hello">Hello, My name is <span class="name">Shariful islam</span></h3>
+						<h3 class="my-profession">I'm a <span class="typing">Web Designer</span></h3>
+						<p>I'm a Web Designer with extencive experience for over 5 years. My expertise is to create & website design, Graphic Design and any more....</p>
+						<a href="#contact" class="btn hire-me btn-danger">Hire Me</a>
+					</div>
+					<div class="home-img col-4 col-sm-12">
+						<img src="img/hero.jpg" alt="">
+					</div>
+
+				</div>
+			</div>
+
+		</section>
+		<!--Home section End-->
+		<!--About section Start-->
+		<section class="about section pt-2 " id="about">
+			<div class="container pt-5 pb-5">
+				<div class="row">
+					<div class="section-title pb-5">
+						<h2>About Me</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="about-content">
+						<div class="row">
+							<div class="about-text">
+								<h3>I'm a Shariful Islam & <span>Web Devloper</span></h3>
+								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, totam aliquam. Saepe quo eius nihil ad, minus debitis reiciendis rerum nisi ex excepturi iure, blanditiis perferendis veniam suscipit libero in pariatur, explicabo incidunt velit nam laudantium quibusdam neque. Facere dignissimos culpa molestiae ipsum illum quis maxime. Blanditiis veniam recusandae perspiciatis? Optio iure modi illum molestiae?</p>
+							</div>
+						</div>
+						<div class="row">
+							<div class="personal-info ">
+									<div class="row">
+										<div class="info-iteam">
+											<p>Birthday :	<span>07 Nov 1994</span></p>
+										</div>
+										<div class="info-iteam">
+											<p>website :	<span>www.shajjad007.xyz</span></p>
+										</div>
+										<div class="info-iteam">
+											<p>Email :	<span>shajjad002@gmail.com</span></p>
+										</div>
+										<div class="info-iteam">
+											<p>Degree :	<span>BSE in COMPUTER SCIENCE & ENGINEERING</span></p>
+										</div>
+										<div class="info-iteam">
+											<p>Phone :	<span>+8801779 197769</span></p>
+										</div>
+										<div class="info-iteam">
+											<p>City :	<span>Chattogram</span></p>
+										</div>
+										<div class="buttons mt-3">
+											<a href="" class="btn ">Download CV</a>
+											<a href="#contact" class="btn hire-me">Hire Me</a>
+										</div>
+									</div>
+								</div>
+							
+							<div class="skills">
+									<div class="row">
+										<div class="skill-iteam">
+											<h5>HTML</h5>
+											<div class="progress">
+												<div class="progress-bar progress-bar-striped bg-danger progress-bar-animated" role="progressbar" aria-label="Animated striped example" aria-valuenow="96" aria-valuemin="0" aria-valuemax="100" style="width: 96%">
+												</div>
+												<h6 class="skill-present">96%</h6>
+											</div>
+										</div>
+										<div class="skill-iteam">
+											<h5>CSS</h5>
+											<div class="progress">
+												<div class="progress-bar progress-bar-striped bg-danger progress-bar-animated" role="progressbar" aria-label="Animated striped example" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+												</div>
+												<h6 class="skill-present">80%</h6>
+											</div>
+										</div>
+										<div class="skill-iteam">
+											<h5>JS</h5>
+											<div class="progress">
+												<div class="progress-bar progress-bar-striped bg-danger progress-bar-animated" role="progressbar" aria-label="Animated striped example" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+												</div>
+												<h6 class="skill-present">60%</h6>
+											</div>
+										</div>
+										<div class="skill-iteam">
+											<h5>Bootstrap</h5>
+											<div class="progress">
+												<div class="progress-bar progress-bar-striped bg-danger progress-bar-animated" role="progressbar" aria-label="Animated striped example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%">
+												</div>
+												<h6 class="skill-present">75%</h6>
+											</div>
+										</div>
+										<div class="skill-iteam">
+											<h5>PHP</h5>
+											<div class="progress">
+												<div class="progress-bar progress-bar-striped bg-danger progress-bar-animated" role="progressbar" aria-label="Animated striped example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%">
+												</div>
+												<h6 class="skill-present">50%</h6>
+											</div>
+										</div>
+										<div class="skill-iteam">
+											<h5>PYTHON</h5>
+											<div class="progress">
+												<div class="progress-bar progress-bar-striped bg-danger progress-bar-animated" role="progressbar" aria-label="Animated striped example" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100" style="width: 10%">
+												</div>
+												<h6 class="skill-present">10%</h6>
+											</div>
+										</div>
+										<div class="skill-iteam">
+											<h5>GRAPHICS DESIGN</h5>
+											<div class="progress">
+												<div class="progress-bar progress-bar-striped bg-danger progress-bar-animated" role="progressbar" aria-label="Animated striped example" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%">
+												</div>
+												<h6 class="skill-present">70%</h6>
+											</div>
+										</div>
+										
+									</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="education">
+									<h3 class="title">Education</h3>
+									<div class="row">
+										<div class="titmline-box">
+											<div class="timline-iteam">
+												<div class="circle-dot"></div>
+												<h3 class="timeline-date">
+													<i class="fa fa-calendar"></i> 2018 - 2022
+												</h3>
+												<h4 class="titmeline-title">BSC in Computer Science & Engineering</h4>
+												<p class="titmeline-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus dolor labore totam ut impedit dolore distinctio, at ea veritatis suscipit odio obcaecati pariatur similique animi tempore?</p>
+											</div>
+											<div class="timline-iteam">
+												<div class="circle-dot"></div>
+												<h3 class="timeline-date">
+													<i class="fa fa-calendar"></i> 2012 - 2016
+												</h3>
+												<h4 class="titmeline-title">Diploma in Computer Engineering </h4>
+												<p class="titmeline-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus dolor labore totam ut impedit dolore distinctio, at ea veritatis suscipit odio obcaecati pariatur similique animi tempore?</p>
+											</div>
+											<div class="timline-iteam">
+												<div class="circle-dot"></div>
+												<h3 class="timeline-date">
+													<i class="fa fa-calendar"></i> 2018 - 2022
+												</h3>
+												<h4 class="titmeline-title">SSC in  Science</h4>
+												<p class="titmeline-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus dolor labore totam ut impedit dolore distinctio, at ea veritatis suscipit odio obcaecati pariatur similique animi tempore?</p>
+											</div>
+										</div>
+									</div>
+							</div>	
+							<div class="experience">
+									<h3 class="title">Experience</h3>
+									<div class="row">
+										<div class="titmline-box">
+											<div class="timline-iteam">
+												<div class="circle-dot"></div>
+												<h3 class="timeline-date">
+													<i class="fa fa-calendar"></i> 2018 - 2022
+												</h3>
+												<h4 class="titmeline-title">BSC in Computer Science & Engineering</h4>
+												<p class="titmeline-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus dolor labore totam ut impedit dolore distinctio, at ea veritatis suscipit odio obcaecati pariatur similique animi tempore?</p>
+											</div>
+											<div class="timline-iteam">
+												<div class="circle-dot"></div>
+												<h3 class="timeline-date">
+													<i class="fa fa-calendar"></i> 2012 - 2016
+												</h3>
+												<h4 class="titmeline-title">Diploma in Computer Engineering </h4>
+												<p class="titmeline-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus dolor labore totam ut impedit dolore distinctio, at ea veritatis suscipit odio obcaecati pariatur similique animi tempore?</p>
+											</div>
+											<div class="timline-iteam">
+												<div class="circle-dot"></div>
+												<h3 class="timeline-date">
+													<i class="fa fa-calendar"></i> 2018 - 2022
+												</h3>
+												<h4 class="titmeline-title">SSC in  Science</h4>
+												<p class="titmeline-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus dolor labore totam ut impedit dolore distinctio, at ea veritatis suscipit odio obcaecati pariatur similique animi tempore?</p>
+											</div>
+										</div>
+									</div>
+							</div>
+							
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		</section>
+		<!--About section End-->
+		<!--Service section Start-->
+		<section class="service section pt-2" id="service">
+			<div class="container pt-5 pb-5">
+				<div class="row">
+					<div class="section-title pb-5">
+						<h2>Services</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="service-iteam">
+							<div class="service-iteam-inner">
+								<div class="icon">
+									<i class="fa fa-mobile-alt"></i>
+								</div>
+								<h4>Web Design</h4>
+								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quaerat omnis laudantium.</p>
+							</div>
+						</div>
+					<div class="service-iteam">
+							<div class="service-iteam-inner">
+								<div class="icon">
+									<i class="fa fa-laptop-code"></i>
+								</div>
+								<h4>Web Design</h4>
+								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quaerat omnis laudantium.</p>
+							</div>
+						</div>
+					<div class="service-iteam">
+							<div class="service-iteam-inner">
+								<div class="icon">
+									<i class="fa fa-palette"></i>
+								</div>
+								<h4>Web Design</h4>
+								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quaerat omnis laudantium.</p>
+							</div>
+						</div>
+					<div class="service-iteam">
+							<div class="service-iteam-inner">
+								<div class="icon">
+									<i class="fa fa-code"></i>
+								</div>
+								<h4>Web Design</h4>
+								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quaerat omnis laudantium.</p>
+							</div>
+						</div>
+					<div class="service-iteam">
+							<div class="service-iteam-inner">
+								<div class="icon">
+									<i class="fa fa-search"></i>
+								</div>
+								<h4>Web Design</h4>
+								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quaerat omnis laudantium.</p>
+							</div>
+						</div>
+					<div class="service-iteam">
+							<div class="service-iteam-inner">
+								<div class="icon">
+									<i class="fa fa-bullhorn"></i>
+								</div>
+								<h4>Web Design</h4>
+								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quaerat omnis laudantium.</p>
+							</div>
+						</div>
+					
+					
+				</div>
+			</div>
+		</section>
+		<!--Service section End-->
+		<!--Protfolio section Start-->
+		<section class="protfolio section pt-2" id="protfolio">
+			<div class="container pt-4 ">
+				<div class="row">
+					<div class="section-title pb-5">
+						<h2>Protfolio</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-12">
+						<div class="protfolio-heading">
+							<h2>My Last Projects : </h2>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="protfolio-iteam">
+						<div class="protfolio-iteam-inner">
+							<div class="protfolio-img">
+								<img src="img/protfolio.png" alt="">
+							</div>
+							<h4>Web Design</h4>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quaerat omnis laudantium.</p>
+						</div>
+					</div>
+					<div class="protfolio-iteam">
+						<div class="protfolio-iteam-inner">
+							<div class="protfolio-img">
+								<img src="img/protfolio1.png" alt="">
+							</div>
+							<h4>Web Design</h4>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quaerat omnis laudantium.</p>
+						</div>
+					</div>
+					<div class="protfolio-iteam">
+						<div class="protfolio-iteam-inner">
+							<div class="protfolio-img">
+								<img src="img/protfolio2.png" alt="">
+							</div>
+							<h4>Web Design</h4>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quaerat omnis laudantium.</p>
+						</div>
+					</div>
+					<div class="protfolio-iteam">
+						<div class="protfolio-iteam-inner">
+							<div class="protfolio-img">
+								<img src="img/protfolio3.png" alt="">
+							</div>
+							<h4>Web Design</h4>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quaerat omnis laudantium.</p>
+						</div>
+					</div>
+					<div class="protfolio-iteam">
+						<div class="protfolio-iteam-inner">
+							<div class="protfolio-img">
+								<img src="img/protfolio4.jpg" alt="">
+							</div>
+							<h4>Web Design</h4>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quaerat omnis laudantium.</p>
+						</div>
+					</div>
+					<div class="protfolio-iteam">
+						<div class="protfolio-iteam-inner">
+							<div class="protfolio-img">
+								<img src="img/protfolio6.png" alt="">
+							</div>
+							<h4>Web Design</h4>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quaerat omnis laudantium.</p>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		</section>
+
+		<!--Protfolio section End-->
+
+	
+
+<?php
+	include('include/footer.php');
+?>
